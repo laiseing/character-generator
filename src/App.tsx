938db6267f3dc94b6707fdbdb4bf632e88351513
Gabriel Geraldeli <ggeraldeli@gmail.com>
2021-02-07 17:00:18 -0300

    config tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> 
         
         <div className="App-logo">
           <a href=".">
