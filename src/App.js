c3ccc41a571f203bbbb1f966b1553f98c4d2e337
Lais Eing <laiseing@gmail.com>
2021-02-07 16:46:27 -0300

    Header
    Logo e link github

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Button } from 'bold-ui'
+import { Theme, useStyles, Button } from 'bold-ui'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div className="App-logo">
+          <a href=".">
+            <img src="/src/resources/happyd20.svg" alt="A flat style d20 with a smile face"/>
+            <h1>CharGenerator</h1>
+          </a>
+        </div>
+        <div className="Github">
+        <a href="https://github.com/laiseing/character-generator" target="_blank">Github</a>
+        </div>
+
+
       </header>
       <body className="App-body">
   
